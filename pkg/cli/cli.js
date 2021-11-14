be1e2f56aa148bd06d567a50c3b90fbb84292e5e
Alexandre Lotte <dev@alexlotte.ca>
2021-11-14 11:57:34 -0500

    CLI improvements

--- pkg/cli/cli.js
+++ pkg/cli/cli.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const path = require('path');
 
-const viteBinary = path.resolve(__dirname, 'node_modules/.bin/vite')
+const viteBinary = path.resolve(__dirname, 'node_modules/.bin/vite');
 const viteConfig = path.resolve(__dirname, 'vite.config.js')
 
-const command = `${viteBinary} --config ${viteConfig}`
-
-const nodeProcess = exec(command)
-
-nodeProcess.stdout.pipe(process.stdout)
+spawn(viteBinary, ['--config', viteConfig], {
+  shell: true,
+  stdio: 'inherit'
+});
