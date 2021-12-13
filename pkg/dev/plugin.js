5a83c006bf1e00bbc147fb17977a7021f1c07b07
Alexandre Lotte <dev@alexlotte.ca>
2021-12-12 19:09:05 -0500

    Fix HMR

--- pkg/dev/plugin.js
+++ pkg/dev/plugin.js
@@ -2,42 +2,74 @@ import fs from 'fs';
 import path from 'path';
 import { defineConfig, mergeConfig } from "vite";
 
-const cliPulsor = path.resolve(__dirname, 'node_modules/@pulsor/core/src');
 const appToCliPath = path.relative(process.cwd(), __dirname);
 const htmlFilePath = path.resolve(__dirname, 'index.html');
 const tempHtmlFilePath = path.resolve(process.cwd(), 'index.html');
 
-const createMainFile = (root) => `import initialAppModule from '${root}';
+// ====
 
-import { run } from '@pulsor/core';
+const sufixesToCheck = [
+  '.js', '.jsx', '.ts', '.tsx',
+  '/index.js', '/index.jsx', '/index.ts', '/index.tsx',
+]
+
+const getExactName = (path) => {
+  if (path.includes('.')) {
+    return path;
+  }
+  for (const suffix of sufixesToCheck) {
+    const str = `${path}${suffix}`
+    if (fs.existsSync(str)) {
+      return str;
+    }
+  }
+  return false;
+}
+
+// ===
+
+const isProject = fs.existsSync(path.resolve(process.cwd(), 'package.json'));
+
+const projectPulsor = '@pulsor/core';
+const cliPulsor = path.resolve(__dirname, 'node_modules/@pulsor/core/src');
+
+const pulsorPath = isProject ? projectPulsor : cliPulsor;
+// const pulsorPath = path.resolve(__dirname, '../core/src');
+
+// ===
+const createMainFile = (rootNode) => `import initialAppModule from '${rootNode}';
+
+import { run } from '${pulsorPath}';
 
 let app = initialAppModule;
 
-// const rootApp = [
-//   () => app,
-//   {
-//     init: {
-//       run: (emit) => {
-//         if (import.meta.hot) {
-//           import.meta.hot.accept('./index', (newModule) => {
-//             app = newModule.default
-//             emit('hrmupdate')
-//           })
-//         }
-//       },
-//       onhrmupdate: (state) => ({ ...state })
-//     }
-//   },
-// ];
+let rootApp = app;
 
-run(app);
+if (import.meta.hot) {
+  rootApp = {
+    init: {
+      run: (emit) => {
+        const handler = () => emit('hrmupdate')
+        window.addEventListener('hmr', handler)
+        return () => window.removeEventListener('hmr', handler)
+      },
+      onhrmupdate: ({ })
+    },
+    children: () => app,
+  };
+
+  import.meta.hot.accept('${rootNode.split('/').at(-1)}', (newModule) => {
+    app = newModule.default
+    dispatchEvent(new CustomEvent("hmr"))
+  })
+}
+
+run(rootApp);
 `
 
 const pulsorDevPlugin = () => {
 
-  const isProject = fs.existsSync(path.resolve(process.cwd(), 'package.json'));
-
-  let root;
+  let rootNode;
   return {
     name: "pulsor-dev",
 
@@ -54,20 +86,14 @@ const pulsorDevPlugin = () => {
     },
 
     config(config, { command }) {
-      root = path.resolve(process.cwd(), config.root || '.');
+      rootNode = getExactName(path.resolve(process.cwd(), config.root || '.'));
 
       Object.assign(config, mergeConfig(config, defineConfig({
         root: process.cwd(),
         esbuild: {
           jsxFactory: 'h',
           jsxFragment: 'Fragment',
-          jsxInject: `import { h, Fragment } from '@pulsor/core'`
-        },
-        resolve: {
-          alias: !isProject
-            ? {
-              '@pulsor/core': cliPulsor,
-            } : {}
+          jsxInject: `import { h, Fragment } from '${pulsorPath}'`
         },
         server: {
           fs: {
@@ -82,14 +108,13 @@ const pulsorDevPlugin = () => {
       }
     },
     resolveId(id) {
-      if (id === './main.ts') {
-        return '/main.ts';
+      if (['/main.ts', './main.ts'].includes(id)) {
+        return '\0/main.ts';
       }
     },
     load(id) {
-      if (id === '/main.ts') {
-        const main = createMainFile(root);
-        return main;
+      if (id === '\0/main.ts') {
+        return createMainFile(rootNode);
       }
     },
     closeBundle() {
